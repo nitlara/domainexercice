0a024a96358c10f99f7fbe8be698f89ab56709fb
nitlara <nitlara@gmail.com>
2020-12-10 09:09:38 +0000

    arreglados algunos problemas, ahora ejectua todo pero sigue sin realizar la segunda parte del ejercicio de substituir es

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var pronoun = ['the','our'];
 var adj = ['great', 'big' ];
-var noun = ['jogger','racoon','puede'];
+var noun = ['jogger','puedes'];
 var domain = ['es','com'];
 let phrase = []
 
@@ -8,27 +8,23 @@ let phrase = []
 for (let i=0; i < pronoun.length;i++)
     {for (let h = 0; h < adj.length; h++)
         {for (let j = 0; j < noun.length; j++)
-            {for (let l = 0; l < domain.length; l++)
-                phrase = 'www.'+ pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
-                console.log(phrase)                
-            }
+            {for (let l = 0; l < domain.length; l++){
+                phrase = 'www.'+ pronoun[i]+adj[h]+noun[j]+'.'+domain[l];
+                console.log(phrase);
+            }}
         }
     }
-
 
 for (let i=0; i < pronoun.length;i++)
     {for (let h = 0; h < adj.length; h++)
         {for (let j = 0; j < adj.length; j++)
-            {for (let l = 0; l < domain.length; l++)
-                if ((j.indexOf(l) !== -1) == true ) 
-                    {j.replace(/es/gi,"");
+            {for (let l = 0; l < domain.length; l++){
+                if ((noun[j].indexOf(domain[l]) !== -1) == true ) 
+                    {noun[j].replace(/es/gi,"");
                     phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
                     } else 
                         { phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
                         }
-                
                 console.log(phrase)                 
-                
-            }
-        }
-    }
+            
+        }}}}
