4921ec25a394a3e29180f8287b2279facfe6ad4c
nitlara <nitlara@gmail.com>
2020-12-10 08:44:59 +0000

    ejercicio ampliado no terminado. propuesta con es de ejemplo en j

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 var pronoun = ['the','our'];
 var adj = ['great', 'big' ];
-var noun = ['jogger','racoon'];
-var domain = ['.es','.com'];
+var noun = ['jogger','racoon','puede'];
+var domain = ['es','com'];
 let phrase = []
+
+
+for (let i=0; i < pronoun.length;i++)
+    {for (let h = 0; h < adj.length; h++)
+        {for (let j = 0; j < noun.length; j++)
+            {for (let l = 0; l < domain.length; l++)
+                phrase = 'www.'+ pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
+                console.log(phrase)                
+            }
+        }
+    }
+
 
 for (let i=0; i < pronoun.length;i++)
     {for (let h = 0; h < adj.length; h++)
         {for (let j = 0; j < adj.length; j++)
             {for (let l = 0; l < domain.length; l++)
-                phrase = pronoun[i]+adj[h]+noun[j]+domain[l]
-                console.log('www.'+ phrase)                
+                if ((j.indexOf(l) !== -1) == true ) 
+                    {j.replace(/es/gi,"");
+                    phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
+                    } else 
+                        { phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
+                        }
+                
+                console.log(phrase)                 
+                
             }
         }
     }
