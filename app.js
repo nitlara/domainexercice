6cb696a2d9ffabc758f5ef63b9ca4ae1d21247f5
nitlara <nitlara@gmail.com>
2020-12-10 09:25:37 +0000

    resuelto!

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var adj = ['great', 'big' ];
 var noun = ['jogger','puedes'];
 var domain = ['es','com'];
 let phrase = []
+var nounB =[]
 
 
 for (let i=0; i < pronoun.length;i++)
@@ -15,16 +16,21 @@ for (let i=0; i < pronoun.length;i++)
         }
     }
 
+console.log("segunda parte")
+
 for (let i=0; i < pronoun.length;i++)
     {for (let h = 0; h < adj.length; h++)
         {for (let j = 0; j < adj.length; j++)
             {for (let l = 0; l < domain.length; l++){
                 if ((noun[j].indexOf(domain[l]) !== -1) == true ) 
-                    {noun[j].replace(/es/gi,"");
-                    phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
+                    { nounB =noun[j].replace(/es/gi,"");
+                    phrase = 'www.'+pronoun[i]+adj[h]+nounB+'.'+domain[l]
                     } else 
                         { phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
                         }
                 console.log(phrase)                 
-            
-        }}}}
+                    }
+                }
+            }
+        }
+        
