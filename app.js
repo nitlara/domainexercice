0d373a81fe2156c4e0cc14bac06d7e163e3fc91b
nitlara <nitlara@gmail.com>
2020-12-10 09:58:25 +0000

    fijando como añadir más de un dominio dentro del mismo for

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var pronoun = ['the','our'];
 var adj = ['great', 'big' ];
-var noun = ['jogger','puedes'];
+var noun = ['jogger','puedes','icom'];
 var domain = ['es','com'];
 let phrase = []
 var nounB =[]
+var nounC =[]
+
 
 
 for (let i=0; i < pronoun.length;i++)
@@ -16,21 +18,39 @@ for (let i=0; i < pronoun.length;i++)
         }
     }
 
-console.log("segunda parte")
+console.log("--------------------segunda parte-------------------")
 
 for (let i=0; i < pronoun.length;i++)
     {for (let h = 0; h < adj.length; h++)
-        {for (let j = 0; j < adj.length; j++)
+        {for (let j = 0; j < noun.length; j++)
             {for (let l = 0; l < domain.length; l++){
                 if ((noun[j].indexOf(domain[l]) !== -1) == true ) 
                     { nounB =noun[j].replace(/es/gi,"");
-                    phrase = 'www.'+pronoun[i]+adj[h]+nounB+'.'+domain[l]
-                    } else 
-                        { phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
-                        }
+                    phrase = 'www.'+pronoun[i]+adj[h]+nounB+'.'+domain[l]}
+                    else
+                    { phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
+                                }
                 console.log(phrase)                 
                     }
                 }
             }
         }
-        
+
+console.log("--------------------tercera parte-------------------")
+
+for (let i=0; i < pronoun.length;i++)
+    {for (let h = 0; h < adj.length; h++)
+        {for (let j = 0; j < noun.length; j++)
+            {for (let l = 0; l < domain.length; l++){
+                if ((noun[j].indexOf(domain[l]) !== -1) == true ) 
+                    { nounC =noun[j].replace(/es/gi,"" ); // como añadir aquí (/com/gi,"")
+                    phrase = 'www.'+pronoun[i]+adj[h]+nounC+'.'+domain[l]}
+                    else
+                    { phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
+                                }
+                console.log(phrase)                 
+                    }
+                }
+            }
+        }
+ 
