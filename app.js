43fccb42a96732add48263a2df2f33ee6194a395
nitlara <nitlara@gmail.com>
2020-12-10 10:51:08 +0000

    domain añadido segun la propia variable domain

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var pronoun = ['the','our'];
 var adj = ['great', 'big' ];
-var noun = ['jogger','puedes','icom'];
-var domain = ['es','com'];
+var noun = ['jogger','puedes','icom', 'poderio'];
+var domain = ['es','com', 'io'];
 let phrase = []
 var nounB =[]
 var nounC =[]
@@ -18,8 +18,8 @@ for (let i=0; i < pronoun.length;i++)
         }
     }
 
-console.log("--------------------segunda parte-------------------")
-
+console.log("--------------------Primer intento replace dominio-------------------")
+//--------------------funciona pero en realidad cuando añades algo que no sea ES no te sirve.
 for (let i=0; i < pronoun.length;i++)
     {for (let h = 0; h < adj.length; h++)
         {for (let j = 0; j < noun.length; j++)
@@ -36,14 +36,15 @@ for (let i=0; i < pronoun.length;i++)
             }
         }
 
-console.log("--------------------tercera parte-------------------")
+console.log("--------------------Segundo intento replace dominio------------------")
+//--------------------¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯
 
 for (let i=0; i < pronoun.length;i++)
     {for (let h = 0; h < adj.length; h++)
         {for (let j = 0; j < noun.length; j++)
             {for (let l = 0; l < domain.length; l++){
                 if ((noun[j].indexOf(domain[l]) !== -1) == true ) 
-                    { nounC =noun[j].replace(/es/gi,"" ); // como añadir aquí (/com/gi,"")
+                    { var nounC = noun[j].replace(domain[l], ""); // añadir es variable 
                     phrase = 'www.'+pronoun[i]+adj[h]+nounC+'.'+domain[l]}
                     else
                     { phrase = 'www.'+pronoun[i]+adj[h]+noun[j]+'.'+domain[l]
@@ -54,3 +55,4 @@ for (let i=0; i < pronoun.length;i++)
             }
         }
  
+
